e37336939928f1a9b1ab4aea82381aa009e63310
Saif Ur Rehman Siddiqui <urrehmansaif406@gmail.com>
2026-03-11 14:36:08 +0500

    Update API baseURL to live Render URL

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000',
+  baseURL: 'https://full-stack-development-assessment.onrender.com/',
   withCredentials: true, 
   headers: {
     'Content-Type': 'application/json',
